b5a8a2053b2813ff8935cc7e34c96ef67226b45a
Suraj Karki <suraj.karki500@gmail.com>
2020-07-25 23:29:36 +0545

    Finished createMany

--- Node-Mongo/connection.js
+++ Node-Mongo/connection.js
@@ -1,6 +1,7 @@
 import mongodb from "mongodb";
 
 import create from "./CRUD/create";
+import createMany from "./CRUD/createMany";
 import listDatabase from "./listDatabase";
 
 export default async function makeDb() {
@@ -13,6 +14,44 @@ export default async function makeDb() {
   try {
     const c = await client.connect();
     listDatabase(c);
+
+    // Create operation
+    // Single document insertion //
+    /*
+    await create(c, {
+      name: "Kings House",
+      summary: "A great house in Nepal.",
+      bedrooms: 2,
+      bathrooms: 2,
+    });
+    */
+
+    // Multiple document insertion //
+    await createMany(client, [
+      {
+        name: "Infinite Views",
+        summary: "Modern home with infinite views from the infinity pool",
+        property_type: "House",
+        bedrooms: 5,
+        bathrooms: 4.5,
+        beds: 5,
+      },
+      {
+        name: "Private room in London",
+        property_type: "Apartment",
+        bedrooms: 1,
+        bathroom: 1,
+      },
+      {
+        name: "Beautiful Beach House",
+        summary:
+          "Enjoy relaxed beach living in this house with a private beach",
+        bedrooms: 4,
+        bathrooms: 2.5,
+        beds: 7,
+        last_review: new Date(),
+      },
+    ]);
   } catch (e) {
     console.log(e);
   } finally {
