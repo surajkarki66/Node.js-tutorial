2218d4c3b2cf4562af83de08802014d30fda8a07
surajkarki66 <suraj.karki500@protonmail.com>
2021-11-23 00:59:39 +0545

    Finished products list endpoint

--- express-prisma-api-crud/routes/api.route.js
+++ express-prisma-api-crud/routes/api.route.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 router.get("/products", async (req, res, next) => {
-  res.send({ message: "Ok api is working 🚀" });
+  try {
+    const products = await prisma.product.findMany({});
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/products/:id", async (req, res, next) => {
